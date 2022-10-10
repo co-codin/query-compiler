1ddf76c0df7f89160701da02237ec4158cbca3a9
agudimov <anton.gudimov@itentika.ru>
2022-10-10 12:55:42 +0300

    Decomposed _parse_query and _build_sql_query functions

--- query_compiler/services/query_parse.py
+++ query_compiler/services/query_parse.py
@@ -1,13 +1,15 @@
 import json
 import logging
 
-from typing import Dict, Tuple, Set, Generator, cast
+from typing import Dict, Tuple, Set, cast, Union, Literal, List
 
 from query_compiler.schemas.attribute import Attribute, Alias, Aggregate
 from query_compiler.schemas.data_catalog import DataCatalog
 from query_compiler.schemas.filter import Filter, SimpleFilter, BooleanFilter
 from query_compiler.schemas.table import Table
 from query_compiler.errors.query_parse_errors import DeserializeJSONQueryError
+
+_result = []
 
 
 def generate_sql_query(query: bytes) -> str:
@@ -40,24 +42,55 @@ def _from_json_to_dict(query: bytes, logger: logging.Logger) -> Dict:
 
 
 def _parse_query(query: Dict, logger: logging.Logger) -> Tuple[
-    Generator[Attribute, None, None],
-    Filter, Generator[Attribute, None, None],
+    List[Attribute],
+    Filter,
+    List[Attribute],
     Filter
 ]:
     """Function for parsing json query and creating schemas objects"""
     logger.info(f"Starting parsing the following query {query}")
 
-    for alias, record in query['aliases'].items():
-        Alias.all_aliases[alias] = Attribute.get(record)
+    _parse_aliases(query, logger)
+    attributes = _parse_attributes(query, logger, key='attributes')
 
-    attributes = (Attribute.get(record) for record in query['attributes'])
-
-    filter_ = Filter.get(query['filter'])
-    groups = (Attribute.get(record) for record in query['group'])
-    having = Filter.get(query['having'])
+    filter_ = _parse_filter(query, logger, key='filter')
+    groups = _parse_attributes(query, logger, key='group')
+    having = _parse_filter(query, logger, key='having')
 
     logger.info(f"Query parsing successfully completed")
     return attributes, filter_, groups, having
+
+
+def _parse_aliases(query: Dict, logger: logging.Logger):
+    try:
+        for alias, record in query['aliases'].items():
+            Alias.all_aliases[alias] = Attribute.get(record)
+    except KeyError:
+        logger.warning(f"There's no aliases in the query {query}")
+
+
+def _parse_attributes(
+        query: Dict,
+        logger: logging.Logger,
+        key: Literal['attributes', 'group']
+) -> Union[List[Attribute], None]:
+    """Parses attributes and group sections of the input query"""
+    try:
+        return [Attribute.get(record) for record in query[key]]
+    except KeyError:
+        logger.warning(f"There's no {key} in the query {query}")
+
+
+def _parse_filter(
+        query: Dict,
+        logger: logging.Logger,
+        key: Literal['filter', 'having']
+) -> Union[Filter, None]:
+    """Parses filter and having sections of the input query"""
+    try:
+        return Filter.get(query[key])
+    except KeyError:
+        logger.warning(f"There's no {key} in the query {query}")
 
 
 def _build_join_hierarchy(logger: logging.Logger) -> Tuple[Table, Set[Table]]:
@@ -76,42 +109,54 @@ def _build_join_hierarchy(logger: logging.Logger) -> Tuple[Table, Set[Table]]:
 
 
 def _build_sql_query(
-        attributes: Generator[Attribute, None, None],
+        attributes: List[Attribute],
         root_table: Table,
         tables: Set[Table],
         filter_: Filter,
-        groups: Generator[Attribute, None, None],
+        groups: List[Attribute],
         having: Filter,
         logger: logging.Logger
 ) -> str:
     """Function for building sql query for a particular database from
     schemas objects"""
     logger.info("Starting building SQL query")
-    result = ['select']
 
-    attributes_to_select = (_get_pg_attribute(attr) for attr in attributes)
+    _build_attributes_clause(attributes, key='select')
+    _build_from_clause(root_table, tables)
+    _build_filter_clause(filter_, key='where')
+    _build_attributes_clause(groups, key='group by')
+    _build_filter_clause(having, key='having')
 
-    result.append(', '.join(attributes_to_select))
-    result.append(f'from {root_table.name}')
-    for join_table in tables:
-        result.append(f'join {join_table.name} on '
-                      f'{join_table.related}.{join_table.related_on[0]} = '
-                      f'{join_table.name}.{join_table.related_on[1]}')
-
-    result.append('where')
-    result.append(_get_pg_filter(filter_))
-
-    if groups:
-        result.append(f'group by')
-        for group in groups:
-            result.append(f'{_get_pg_attribute(group)}')
-
-    result.append('having')
-    result.append(_get_pg_filter(having))
-
-    result = ' '.join(result)
+    result = ' '.join(_result)
     logger.info("SQL query building successfully completed")
     return result
+
+
+def _build_attributes_clause(
+        attributes: List[Attribute],
+        key: Literal['select', 'group by']
+):
+    """Builds select and group by clauses"""
+    if attributes:
+        _result.append(key)
+        attributes_to_append = (_get_pg_attribute(attr) for attr in attributes)
+        _result.append(', '.join(attributes_to_append))
+
+
+def _build_from_clause(root_table: Table, tables: Set[Table]):
+    """Builds from and join clauses"""
+    _result.append(f'from {root_table.name}')
+    for join_table in tables:
+        _result.append(f'join {join_table.name} on '
+                       f'{join_table.related}.{join_table.related_on[0]} = '
+                       f'{join_table.name}.{join_table.related_on[1]}')
+
+
+def _build_filter_clause(filter_: Filter, key: Literal['where', 'having']):
+    """Builds where and having clauses"""
+    if filter_:
+        _result.append(key)
+        _result.append(_get_pg_filter(filter_))
 
 
 """
