49fb1c04ed8e49c7f81f9ddb969c90823e1793df
a.gudimov <anton.gudimov@itentika.ru>
2023-08-18 15:10:00 +0300

    removed casting values in filter

--- query_compiler/schemas/filter.py
+++ query_compiler/schemas/filter.py
@@ -10,9 +10,7 @@ from query_compiler.configs.settings import settings
 from query_compiler.schemas.attribute import Aggregate, Alias
 from query_compiler.schemas.data_catalog import DataCatalog
 from query_compiler.errors.schemas_errors import (
-    FilterConvertError, FilterValueCastError, UnknownAggregationFunctionError,
-    UnknownOperatorFunctionError
-)
+    FilterConvertError, UnknownAggregationFunctionError, UnknownOperatorFunctionError)
 
 LOG = logging.getLogger(__name__)
 
@@ -40,15 +38,15 @@ class BooleanFilter(Filter):
 
 
 class SimpleFilter(Filter):
-    _type_names_to_types = {
-        'int': lambda val: val if isinstance(val, int) else int(val),
-        'float': lambda val: val if isinstance(val, float) else float(val),
-        'str': lambda val: val if isinstance(val, str) else str(val),
-        'bool': lambda val: val if isinstance(val, bool) else bool(val),
-        'date': lambda val: val if isinstance(val, date) else datetime.strptime(val, '%Y-%m-%d').date(),
-        'datetime': lambda val: val if isinstance(val, datetime) else datetime.strptime(val, '%Y-%m-%d %H:%M:%S'),
-        'tuple': lambda val: SimpleFilter._convert_to_tuple(val)
-    }
+    # _type_names_to_types = {
+    #     'int': lambda val: val if isinstance(val, int) else int(val),
+    #     'float': lambda val: val if isinstance(val, float) else float(val),
+    #     'str': lambda val: val if isinstance(val, str) else str(val),
+    #     'bool': lambda val: val if isinstance(val, bool) else bool(val),
+    #     'date': lambda val: val if isinstance(val, date) else datetime.strptime(val, '%Y-%m-%d').date(),
+    #     'datetime': lambda val: val if isinstance(val, datetime) else datetime.strptime(val, '%Y-%m-%d %H:%M:%S'),
+    #     'tuple': lambda val: SimpleFilter._convert_to_tuple(val)
+    # }
 
     @staticmethod
     def _convert_to_tuple(value: str) -> tuple[Any, ...]:
@@ -70,25 +68,18 @@ class SimpleFilter(Filter):
 
     @value.setter
     def value(self, value):
-        if self.operator == 'is null':
-            self._value = None
-        else:
-            attr_type_name = self._get_attr_type_name()
-            try:
-                self._value = self._type_names_to_types.get(attr_type_name)(value)
-                if attr_type_name != 'tuple':
-                    self._value = sql.quote(self._value)
-                match self.operator:
-                    case 'in':
-                        self._value = f"({','.join(map(sql.quote, self._value))})"
-                    case 'between':
-                        left, right = self._value
-                        self._value = f"{sql.quote(left)} and {sql.quote(right)}"
-                    case 'like':
-                        if not isinstance(self._value, str):
-                            raise TypeError
-            except (TypeError, ValueError) as exc:
-                raise FilterValueCastError(attr_type_name, value) from exc
+        match self.operator:
+            case 'is null':
+                self._value = None
+            case 'in' | 'between':
+                self._value = self._convert_to_tuple(value)
+                if self._operator == 'in':
+                    self._value = f"({','.join(map(sql.quote, self._value))})"
+                else:
+                    left, right = self._value
+                    self._value = self._value = f"{sql.quote(left)} and {sql.quote(right)}"
+            case _:
+                self._value = sql.quote(value)
 
     @property
     def operator(self):
